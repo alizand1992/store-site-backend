a23c2f28180e769dc73d8f873d195d94817f7947
Ali Zand <ali.zand@realpage.com>
2020-05-21 22:49:32 -0700

    get all items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
   def index
+    items = Item.all
 
+    render json: items.to_json, status: :ok
+  rescue => e
+    render json: { error: e.message }.to_json, status: :internal_server_error
   end
 end
