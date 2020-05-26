4a5ac346a1ea9754012762668c050f97af754920
Ali Zand <ali.zand@realpage.com>
2020-05-25 20:04:08 -0700

    Add More properties

---
+++ db/migrate/20200526030137_add_menu_naming_properties.rb
@@ -0,0 +1,17 @@
+class AddMenuNamingProperties < ActiveRecord::Migration[6.0]
+  def change
+    SiteProperty.new(
+      name: 'MENU_1',
+      value: 'Home',
+      description: 'Title for menu 1',
+      value_type: SiteProperty::TEXT
+    ).save!
+
+    SiteProperty.new(
+      name: 'MENU_2',
+      value: 'Services',
+      description: 'Title for menu 2',
+      value_type: SiteProperty::TEXT
+    ).save!
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_220013) do
+ActiveRecord::Schema.define(version: 2020_05_26_030137) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
